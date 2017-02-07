b1f98f5b3d899eaf10f2466265c21c897435c1c1
Victor Lopes <contato_victorlopes@hotmail.com>
2017-02-07 15:31:16 -0300

    Iniciando projeto

---
+++ app/src/main/java/br/com/bytegod/android/agendaandroid/ActMain.java
@@ -0,0 +1,13 @@
+package br.com.bytegod.android.agendaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
