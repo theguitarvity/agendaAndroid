9ad9cc00da2ebd27d22fc6dbc054d405f3db03e4
Victor Lopes <contato_victorlopes@hotmail.com>
2017-02-07 16:24:15 -0300

    Adicionando layouts

---
+++ app/src/main/java/br/com/bytegod/android/agendaandroid/ActCadContato.java
@@ -0,0 +1,13 @@
+package br.com.bytegod.android.agendaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActCadContato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_cad_contato);
+    }
+}
